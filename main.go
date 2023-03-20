0822bee009938d62bc999f94981d74ce76a2bff1
mtbstn24 <thushibstn24@gmail.com>
2023-03-20 23:52:54 +0530

    add writeProcess, readProcess functions

--- main.go
+++ main.go
@@ -29,13 +29,50 @@ func writeProcess(fileSize int) {
 
 	writeDurations = make([]map[string]interface{}, 0)
 	testData := make([]byte, fileSize)
+	var sum float64
 
-	fmt.Println("Start file write")
-	startTime := time.Now()
-	os.WriteFile(filePath, testData, os.ModePerm)
-	writeDuration = time.Since(startTime).Seconds() * 1000
+	for i := 0; i < 10; i++ {
+		startTime := time.Now()
+		os.WriteFile(filePath, testData, os.ModePerm)
+		writeDuration = time.Since(startTime).Seconds() * 1000
 
+		writeDurations = append(writeDurations, map[string]interface{}{
+			"size":          fileSize,
+			"writeDuration": writeDuration,
+		})
+
+		sum = sum + writeDuration
+	}
+
+	writeDuration = sum / 10
+
+	fmt.Println(writeDurations)
 	fmt.Printf("FileSize (KB): %d, AvgDuration (ms): %f\n", fileSize/1024, writeDuration)
+}
+
+func readProcess(fileSize int) {
+	filePath = filepath.Join(fileDir, fmt.Sprintf("file-%d", fileSize))
+
+	readDurations = make([]map[string]interface{}, 0)
+	var sum float64
+
+	for i := 0; i < 10; i++ {
+		startTime := time.Now()
+		os.ReadFile(filePath)
+		readDuration = time.Since(startTime).Seconds() * 1000
+
+		readDurations = append(readDurations, map[string]interface{}{
+			"size":         fileSize,
+			"readDuration": readDuration,
+		})
+
+		sum = sum + readDuration
+	}
+
+	readDuration = sum / 10
+
+	fmt.Println(readDurations)
+	fmt.Printf("FileSize (KB): %d, AvgDuration (ms): %f\n", fileSize/1024, readDuration)
 }
 
 func main() {
@@ -54,9 +91,13 @@ func main() {
 
 	http.HandleFunc("/file", func(w http.ResponseWriter, r *http.Request) {
 		writeProcess(minfileSize)
-		fmt.Fprintf(w, "FileSize (KB): %d, AvgDuration (ms): %f\n", minfileSize/1024, writeDuration)
+		fmt.Fprintf(w, "FileSize (KB): %d, AvgWriteDuration (ms): %f\n", minfileSize/1024, writeDuration)
+		readProcess(minfileSize)
+		fmt.Fprintf(w, "FileSize (KB): %d, AvgReadDuration (ms): %f\n", minfileSize/1024, readDuration)
 		writeProcess(maxFileSize)
-		fmt.Fprintf(w, "FileSize (KB): %d, AvgDuration (ms): %f\n", maxFileSize/1024, writeDuration)
+		fmt.Fprintf(w, "FileSize (KB): %d, AvgWriteDuration (ms): %f\n", maxFileSize/1024, writeDuration)
+		readProcess(maxFileSize)
+		fmt.Fprintf(w, "FileSize (KB): %d, AvgReadDuration (ms): %f\n", maxFileSize/1024, readDuration)
 	})
 
 	http.ListenAndServe(":8080", nil)
