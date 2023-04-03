795b12928ac9b2b7365187fd8ffac000934ae00f
mtbstn24 <thushibstn24@gmail.com>
2023-04-03 12:18:15 +0530

    add endpoint desc, reduce json out length

--- main.go
+++ main.go
@@ -235,7 +235,7 @@ func main() {
 			fmt.Println("Error resolving hostname:", err)
 			return
 		} else {
-			fmt.Fprintf(w, "Connection successful to the host: %s \nUse the /file endpoint to Benchmark the File oprations \nUse the /response endpoint to get the csv string of the response of Benchmarking the File oprations\nUse the /json endpoint to get static JSON content \nUse the /externalapi endpoint to get a sample json response from an external API\n\n", name)
+			fmt.Fprintf(w, "Connection successful to the host: %s \nUse the /file endpoint to Benchmark the File oprations \nUse the /response endpoint to get the csv string of the response of Benchmarking the File oprations\nUse the /json endpoint to get static JSON content \nUse the /externalapi endpoint to get a sample json response from an external API \nUse the /fibanacci/n endpoint to get the nth fibonacci number and Duration\n\n", name)
 		}
 
 	})
@@ -245,51 +245,6 @@ func main() {
 }
 
 var sampleJson []map[string]interface{} = []map[string]interface{}{
-	{
-		"_id":        "641bdaaaf8e98d12763f22d4",
-		"index":      0,
-		"guid":       "b06a15d1-526a-473f-9209-e0ca22b7f90a",
-		"isActive":   false,
-		"balance":    "$2,086.82",
-		"picture":    "http://placehold.it/32x32",
-		"age":        40,
-		"eyeColor":   "green",
-		"name":       "Arline Dudley",
-		"gender":     "female",
-		"company":    "LUNCHPOD",
-		"email":      "arlinedudley@lunchpod.com",
-		"phone":      "+1 (987) 549-3761",
-		"address":    "111 Moore Street, Walton, Texas, 8265",
-		"about":      "Ad nulla eiusmod voluptate laborum in consectetur mollit pariatur officia aliqua adipisicing est ad. Quis anim aliqua exercitation sit cillum irure nisi quis labore. Dolor nulla enim elit qui amet ipsum exercitation. Eiusmod adipisicing culpa dolore duis est voluptate nulla. Qui sint irure qui irure excepteur laborum pariatur ipsum ipsum Lorem ut irure anim.\r\n",
-		"registered": "2022-06-09T10:09:42 -06:-30",
-		"latitude":   -78.980394,
-		"longitude":  33.205681,
-		"tags": []string{
-			"deserunt",
-			"officia",
-			"id",
-			"pariatur",
-			"sunt",
-			"excepteur",
-			"consectetur",
-		},
-		"friends": []map[string]interface{}{
-			{
-				"id":   0,
-				"name": "Julianne Wright",
-			},
-			{
-				"id":   1,
-				"name": "David Kane",
-			},
-			{
-				"id":   2,
-				"name": "Todd Holman",
-			},
-		},
-		"greeting":      "Hello, Arline Dudley! You have 7 unread messages.",
-		"favoriteFruit": "banana",
-	},
 	{
 		"_id":        "641bdaaa0a5b4af1047d0dde",
 		"index":      1,
